c329596390062937b5b9044832ecc7bff73b3b57
TakanariShimbo <takanari.shimbo@gmail.com>
2024-06-24 17:00:43 +0900

    hide streamlit style

--- view/config.py
+++ view/config.py
@@ -11,3 +11,12 @@ class AppConfig:
             page_icon=LOGO_SVG,
             layout="centered",
         )
+
+        # Hide Streamlit Style
+        hide_streamlit_style = """
+            <style>
+            [data-testid="stToolbar"] {visibility: hidden !important;}
+            footer {visibility: hidden !important;}
+            </style>
+        """
+        st.markdown(hide_streamlit_style, unsafe_allow_html=True)
